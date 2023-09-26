931cc3efdade815f9f2222c24260a3da4ed3e3a1
peachyxxx <kulanbekov519@gmail.com>
2023-09-26 17:05:24 +0600

    ds

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <img src="https://picsum.photos/200/300" alt="random" style={{ borderRadius: '20px' }} />
+      </div>
+      <h1>Hello World!</h1>
+      <h2>just reload page, and you'll see the magic</h2>
+    </>
+  )
+}
+
+export default App
